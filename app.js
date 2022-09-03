cac41433429317fc086eaf7edb54d66f8d8925f0
Bilel Dhifaoui <dhifaoui.bilel1384@gmail.com>
2022-09-03 19:12:24 +0100

    Level 5: Cookies and Sessions

--- app.js
+++ app.js
@@ -5,30 +5,38 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const encrypt = require('mongoose-encryption');   //AES cipher
-// const md5 = require('md5');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+// passport-local, a package which provides a strategy for authenticating with a username and password
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/secretDB");
 const userSchema = new mongoose.Schema({
-  email: {
-     type: String,
-     required: true
-   },
-  password: {
-    type: String,
-    required: true
-  }
+  email: String,
+  password: String
 })
 
+userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', userSchema);
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.route('/').get((req, res) =>{
   res.render('home');
@@ -40,26 +48,30 @@ app.route('/login')
   })
 
   .post((req, res)=>{
-    email = req.body.username;
-    password = req.body.password;
-    User.findOne({ email: email}, (err, user) =>{
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    req.login(user, function(err){
       if(err){
         console.log(err);
       } else {
-        if(user){
-          bcrypt.compare(password, user.password, function(err, result){
-            if(result === true){
-              res.render('secrets');
-            } else {
-              res.send('Password is incorrect!')
-            }
-          })
-        } else {
-          res.send('email is incorrect');
-        }
+        passport.authenticate("local")(req, res,function(){
+          res.redirect('/secrets');
+        });
       }
     })
+
   })
+
+  //// Or use this code
+  // .post(passport.authenticate("local",{
+  //   successRedirect: "/secrets",
+  //   failureRedirect: "/login"
+  //   }), function(req, res){
+  //
+  // });
 
 
 app.route('/register')
@@ -68,17 +80,31 @@ app.route('/register')
   })
 
   .post((req, res) =>{
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-      newUser = new User({email: req.body.username, password: hash});
-      newUser.save((err) =>{
-        if(err){
-          console.log(err);
-        } else {
-          res.render('secrets');
-        }
-      });
-    });
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+      if(err){
+        console.log(err);
+        res.redirect('/register');
+      } else {
+        passport.authenticate('local')(req, res, function(){
+          res.redirect('/secrets');
+        })
+      }
+    })
+  })
 
+app.route('/secrets')
+  .get((req, res)=>{
+    if(req.isAuthenticated()) {
+      res.render('secrets');
+    } else {
+      res.redirect('/login');
+    }
+  })
+
+app.route('/logout')
+  .get((req, res)=>{
+    req.logout();
+    res.redirect('/');
   })
 
 app.listen(3000, ()=>{
